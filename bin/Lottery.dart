9a3cf6ab2f42323748315aedcabb8d3d8432c4c1
flutter-lord <richardolojede2@gmail.com>
2024-09-07 15:54:11 +0100

    commit

--- bin/Lottery.dart
+++ bin/Lottery.dart
@@ -3,6 +3,14 @@ import 'dart:math';
 
 void main() {
 
+   int random = 151; //100 + Random().nextInt(900);
+
+    print('The real number is $random');
+  
+  int r1 = random ~/ 100;
+  int r2 = (random ~/ 10) % 10;
+  int r3 = random % 10;
+
   stdout.write('Enter three digits value: ');
   int number = int.parse(stdin.readLineSync()!);
 
@@ -10,10 +18,19 @@ void main() {
   int n2 = (number ~/ 10) % 10;
   int n3 = number % 10; 
 
-  print('$n1, $n2, $n3');
-  
 
-
-  //int random = 100 + Random().nextInt(900);
-  
+  if (random == number) {
+    print('Congratulations You\'ve won \$10,000');
   }
+  else if ((r1 == n1 || r1 == n2 || r1 == n3) && (r2 == n1 || r2 == n2 || r2 == n3) && (r3 == n1 || r3 == n2 || r3 == n3)) {
+    print('Congratulations You\'ve won \$3,000');
+  }
+  else if ((r1 == n1 && n1 != n2 && n1 != n3) && (r1 == n2 && n2 != n1 && n2 != n3) && (r1 == n3 && n3 != n2 && n3 != n1) ||
+            (r2 == n1 && n1 != n2 && n1 != n3) && (r2 == n2 && n2 != n1 && n2 != n3) && (r2 == n3 && n3 != n2 && n3 != n1) ||
+            (r3 == n1 && n1 != n2 && n1 != n3) && (r3 == n2 && n2 != n1 && n2 != n3) && (r3 == n3 && n3 != n2 && n3 != n1)) {
+               print('Congratulations You\'ve won \$1,000');
+  }
+  else {
+    print('Dear Gambler,No match today You lose Try again later');
+  }
+}
---
+++ bin/random.dart
@@ -0,0 +1,6 @@
+import 'dart:math';
+void main() {
+    int random = 100 + Random().nextInt(900);
+
+  print ('$random');
+}
